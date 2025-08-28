bd9f66f4c605e611432471b2d74cb1149fb6a5b8
mikalaytsepl <kolyatepl@gmail.com>
2025-08-28 13:05:22 +0200

    initial

---
+++ tester_parser.py
@@ -0,0 +1,29 @@
+import os
+import subprocess as sub
+
+def check_for_upgrades()->bool:
+    test = sub.run(["sudo","apt","update"],
+                capture_output=True,
+                text=True).stdout
+
+    manager_status = test.splitlines()[-1]
+    
+    if manager_status != "All packages are up to date.":
+        print(
+            f"There are {manager_status.split()[0]} packages to upgrade"
+            )
+
+
+while True:
+    choice = input(f'Hi, {os.getenv("USER")}! Want to check for any updates?\nY/N:')
+    match choice:
+        case "Y" | "y":
+            print("Updating packages information...")
+            check_for_upgrades()
+            break
+        case "N" | "n":
+            print("Update proposition declined.")
+            break
+        case _:
+            print("Please, specify only Y(y) or N(n).")
+
